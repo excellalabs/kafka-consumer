ea5c70ce26f78e048dca3c40c9bf2e905741e718
Ali Jafari <ali.jafari@excella.com>
2019-02-20 16:04:12 -0500

    add jmx exporter

--- containers/kafka_consumer/run-server.sh
+++ containers/kafka_consumer/run-server.sh
@@ -4,4 +4,4 @@ project_name="kafka-consumer"
 jar_dir="/opt/${project_name}"
 jar_path="${jar_dir}/${project_name}.jar"
 
-/usr/bin/java -jar $jar_path
+/usr/bin/java -javaagent:/opt/kafka-consumer/jmx_prometheus_javaagent.jar=7071:/opt/kafka-consumer/prometheus-config.yml -jar $jar_path
